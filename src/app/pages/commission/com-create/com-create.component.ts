bf946a4343adf63a60094da5cae146e35b4fea04
ramantila <65ntila@gmail.com>
2023-12-14 16:45:14 +0300

    add commision

--- src/app/pages/commission/com-create/com-create.component.ts
+++ src/app/pages/commission/com-create/com-create.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiHttpClient } from '../../../api-client';
 
 @Component({
   selector: 'app-com-create',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './com-create.component.css'
 })
 export class ComCreateComponent {
+  commission: any;
 
+  private http: ApiHttpClient;
+
+  constructor(private httpClient: HttpClient, private router: Router){
+    this.http = new ApiHttpClient(httpClient)
+  }
+
+  onSubmit(commissionForm: NgForm): void{
+    this.http.post(
+      "add/commission",
+      commissionForm.value
+    )
+    .subscribe(
+      (data: any) => {
+        this.commission = data;
+        console.log(this.commission);
+        this.router.navigate(['/commissions/view']);
+      },
+      (error: any) => {
+        console.error('Error fetching debts:', error);
+      }
+    )
+  }
 }
